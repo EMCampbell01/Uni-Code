1383cceef58e8614b48baa38c49ee98ff1b1038f
Euan Campbell <75681738+EMCampbell01@users.noreply.github.com>
2021-10-07 06:28:45 +0100

    Update Q4.sql

--- DatabaseSystems/Part1/Q4.sql
+++ DatabaseSystems/Part1/Q4.sql
@@ -1,1 +1,10 @@
-
+SELECT name
+FROM food_item
+WHERE price >
+ (SELECT price
+  FROM food_item
+  WHERE name = 'Grilled Tamarind Duck')
+AND price <
+ (SELECT price
+  FROM food_item
+  WHERE name = 'Gnocchi alla Sorrentina');
