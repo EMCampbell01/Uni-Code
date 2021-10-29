6349e4dec81d2a7aaaad4d8ccf42918c8b523c77
Euan Campbell <75681738+EMCampbell01@users.noreply.github.com>
2021-10-29 12:44:46 +0100

    Update Q2.sql

--- DatabaseSystems/Part1/Q2.sql
+++ DatabaseSystems/Part1/Q2.sql
@@ -1,3 +1,3 @@
-SELECT name, COUNT (payment_method_id)
+SELECT name, COUNT(payment_method_id)
 FROM orders LEFT JOIN payment_method ON orders.payment_method_id = payment_method.id
-GROUP BY name;
+GROUP BY name
